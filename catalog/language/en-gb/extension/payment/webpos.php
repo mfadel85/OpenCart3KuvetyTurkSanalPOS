0088a89fd94c62d5367e89879d4ebb7bf97df5db
mfadel85 <faddel.homsi@gmail.com>
2020-11-21 22:33:52 +0300

    first commit

---
+++ catalog/language/en-gb/extension/payment/webpos.php
@@ -0,0 +1,36 @@
+<?php
+
+$_['heading_title']     = 'Thank you for shopping with %s . ';
+
+
+// Text
+$_['text_title']           = 'Credit Card (+1,59%) Bank Commision ';
+$_['text_credit_card']     = 'Credit Card Information';
+$_['text_wait']            = 'Please Wait!';
+$_['text_instalments']     = 'Taksitler';
+$_['text_instalment']      = ' Taksit ';
+$_['text_no_instalment']   = ' Tek Çekim(Tüm Bankalar) : ';
+$_['text_3d_hosting']   = ' "Siparişi Onayla" tuşuna bastıktan sonra yönlendirileceğiniz banka Güvenli Ödeme Sayfasında Kredi kartı bilgilerinizi girerek siparişinizi tamamlayacaksınız. Ödeme işlemi tamamlanana kadar tarayıcınızı kapatmayınız.';
+$_['text_single_positive'] = 'Tek Çekim Komisyonu';
+$_['text_single_negative'] = 'Tek Çekim İndirimi';
+$_['text_no_commision'] = 'Sıfır Komisyon';
+
+$_['text_response']     = 'The Results of the payment process:';
+$_['text_success']      = '... successly has been paid.';
+$_['text_success_wait'] = '<b><span style="color: #FF0000">Lütfen Bekleyiniz...</span></b> siparişiniz işleniyor.<br>tarayıcınız 10 saniye içinde sonuç sayfasına yönlendirilmezse <a href="%s">burayı tıklayın</a>.';
+$_['text_failure']      = '... ödeme işlemi başarısız!';
+$_['text_failure_wait'] = '<b><span style="color: #FF0000">Lütfen Bekleyiniz...</span></b><br>tarayıcınız 10 saniye içinde ödeme sayfasına yönlendirilmezse <a href="%s">burayı tıklayın</a>.';
+// Entry
+$_['entry_cc_owner']       = 'Name on Card:';
+$_['entry_cc_number']      = 'Card Number:';
+$_['entry_cc_expire_date'] = 'Validity Date:';
+$_['entry_cc_cvv2']        = 'CVV2:';
+
+//Error
+$_['error_fix']       = 'Please check the erros:';
+$_['error_cc_owner']  = 'Name on card is missing.';
+$_['error_cc_number'] = 'Please check the card number';
+$_['error_cc_cvv2']   = 'CVV2 has to be 3-4 digits';
+$_['error_cc_expire_date'] = 'Validity date is invalid';
+$_['error_cc_number_luhn'] = 'Please check the card number';
+?>
